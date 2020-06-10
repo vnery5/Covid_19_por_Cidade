a3a976e12e8efd0c7ce088010b60f1ae89662e26
vnery5 <vnery5@gmail.com>
2020-06-10 10:29:45 -0300

    09/06/2020

--- app.py
+++ app.py
@@ -19,12 +19,12 @@ df = pd.read_csv("https://raw.githubusercontent.com/vnery5/Covid_19_por_Cidade/m
 
 ##limpando a base de dados
 #renomeando as colunas
-df.rename({'populacaoTCU2019':'populacao','casosAcumulado':'Casos','obitosAcumulado;;':'Óbitos','data':'Data'}, axis = 1, inplace = True)
+df.rename({'populacaoTCU2019':'populacao','casosAcumulado':'Casos','obitosAcumulado':'Óbitos','data':'Data'}, axis = 1, inplace = True)
 #transformando a coluna de data para o tipo apropriado
 df['Data'] = pd.to_datetime(df['Data'])
 #arrumando a coluna de óbitos
-df['Óbitos'] = df.Óbitos.str[:-2]
-df['Óbitos'] = df['Óbitos'].astype('int')
+#df['Óbitos'] = df.Óbitos.str[:-2]
+#df['Óbitos'] = df['Óbitos'].astype('int')
 #limpando as linhas sem indicação de população
 df.dropna(subset = ['populacao'], axis = 0, inplace = True)
 
@@ -154,7 +154,7 @@ app.layout = html.Div(
                                 ),
                                 html.P(
                                     """Criado com Python usando os dados mais recentes do Ministério da Saúde. 
-                                    Atualizado em 09/06/2020."""
+                                    Atualizado em 10/06/2020."""
                                 ),
                             ],
                         ),
