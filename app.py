42ed9863bd06522d69d940a16867c8ecbd232503
vnery5 <vnery5@gmail.com>
2020-06-11 14:15:09 -0300

    same as before but it works now

--- app.py
+++ app.py
@@ -511,25 +511,25 @@ def Atualizar(n_clicks,cidade,estado,opcao):
         
         #calculando os indicadores e formatando as numerações usando o módulo locale
         novos_casos = f"{int(df_cidade['Casos'].tail(1)) - int(df_cidade['Casos'].tail(2).head(1)):n}"
-        novos_casos.replace(",",".")
+        novos_casos = novos_casos.replace(",",".")
 
         novos_obitos = f"{int(df_cidade['Óbitos'].tail(1)) - int(df_cidade['Óbitos'].tail(2).head(1)):n}"
-        novos_obitos.replace(",",".")
+        novos_obitos = novos_obitos.replace(",",".")
 
         incidencia = f"{np.around(num_de_casos*100000/int(df_cidade['populacao'].head(1)),2):n}"
-        incidencia.replace(".",",")
+        incidencia = incidencia.replace(".",",")
 
         mortalidade = f"{np.around(num_de_mortes*100000/int(df_cidade['populacao'].head(1)),2):n}"
-        mortalidade.replace(".",",")
+        mortalidade = mortalidade.replace(".",",")
 
         letalidade = f"{np.around(num_de_mortes/num_de_casos*100,2):n}%"
-        letalidade.replace(".",",")
+        letalidade = letalidade.replace(".",",")
 
         num_de_casos = f"{num_de_casos:n}"
-        num_de_casos.replace(",",".")
+        num_de_casos = num_de_casos.replace(",",".")
 
         num_de_mortes = f"{num_de_mortes:n}"
-        num_de_mortes.replace(",",".")
+        num_de_mortes = num_de_mortes.replace(",",".")
 
         return fig, erro, novos_casos, num_de_casos, incidencia, novos_obitos, num_de_mortes, mortalidade, letalidade
 
