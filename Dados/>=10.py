5c39a280645e9f94a9f9e1fb59da91e1db79d7a6
vnery5 <vnery5@gmail.com>
2020-08-02 22:01:34 -0300

    Novo arquivo para limpar os dados

--- Dados/>=10.py
+++ Dados/>=10.py
@@ -1,8 +1,10 @@
 import pandas as pd
-import os
+import os, glob
 
 #lendo o arquivo csv (já editado pelo script do VBA)
 df = pd.read_csv('teste.csv')
+
+print(df.head(5))
 
 #arrumando uns erros e só pegando as datas com pelo menos um caso
 df = df[df.casosAcumulado > 9]
@@ -17,8 +19,21 @@ df['obitosAcumulado'] = df['obitosAcumulado'].astype('int')
 df['casosNovos'] = df['casosNovos'].astype('int')
 df['obitosNovos'] = df['obitosNovos'].astype('int')
 
-#exportando pra CSV
-df.to_csv(os.path.expanduser('~/Downloads/GitHub/Covid_19_por_Cidade/Dados/dataset_covid_19.csv'), index = False)
-
 #vendo quantas linhas tem
 print(df.count)
+
+#exportando pra CSV e deletando a base de dados antiga
+if os.path.isfile(os.path.expanduser('~/Downloads/GitHub/Covid_19_por_Cidade/Dados/dataset_covid_19.csv')):
+    os.remove(os.path.expanduser('~/Downloads/GitHub/Covid_19_por_Cidade/Dados/dataset_covid_19.csv'))
+else:
+    print("Não foi possível achar o arquivo a ser excluído")
+df.to_csv(os.path.expanduser('~/Downloads/GitHub/Covid_19_por_Cidade/Dados/dataset_covid_19.csv'), index = False)
+
+#apagando o arquivo gerado pelo script do VBA
+os.remove('teste.csv')
+
+#apagando o arquivo do site do ministério da saúde
+for arquivo in glob.glob(os.path.expanduser("~/Downloads/HIST_PAINEL*")):
+    os.remove(arquivo)
+
+print("Tudo certo!")
